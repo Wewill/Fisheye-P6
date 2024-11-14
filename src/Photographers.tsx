816e5e61d36483ee78e8a2484866883e040b91e6
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-11-14 15:46:51 +0100

    Use one state only, use init to load data one time

--- src/Photographers.tsx
+++ src/Photographers.tsx
@@ -1,37 +1,61 @@
-import { useState, useEffect } from 'react';
-import { Data } from './types';
+import { useState, useEffect, useRef } from 'react';
+import { Data } from './types/data';
+
+type State = {
+  data?: Data;
+  loading: boolean;
+  error?: string;
+}
 
 function Photographers() {
-  const [data, setData] = useState<Data | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const initRef = useRef(false);
+  const [state, setState] = useState<State>({loading:true});
 
   useEffect(() => {
+    // Version B 
+    // const fetchData = async () => {
+    //   try {
+    //     const response = await fetch('./data/photographers.json');
+    //     if (!response.ok) { 
+    //       throw new Error(`HTTP Error status: ${response.status}`);
+    //     }
+    //     //console.log(response);
+    //     const data:Data = await response.json();
+    //     setState({data, loading:false} );
+    //   } catch (error) {
+    //     setState({error: error instanceof Error ? error.message : "An error occurred", loading:false});
+    //   }
+    // };
+    // if ( initRef.current === false ) {
+    //   initRef.current = true;
+    //   fetchData();   
+    // };
+
+    // Version A 
+    if ( initRef.current === true ) return;
     const fetchData = async () => {
       try {
         const response = await fetch('./data/photographers.json');
         if (!response.ok) { 
           throw new Error(`HTTP Error status: ${response.status}`);
         }
-        console.log(response);
+        //console.log(response);
         const data:Data = await response.json();
-        setData(data);
+        setState({data, loading:false} );
       } catch (error) {
-        setError(error instanceof Error ? error.message : "An error occurred");
-      } finally {
-        setLoading(false);
+        setState({error: error instanceof Error ? error.message : "An error occurred", loading:false});
       }
     };
-
-    fetchData(); 
+    initRef.current = true;
+    fetchData();   
   }, []);  // Tableau de dépendances vide pour n’exécuter l’effet qu'une fois
 
-  if (loading) return <p>Chargement...</p>;
-  if (error) return <p className='error'>Erreur: {error}</p>;
+  if (state.loading) return <p>Chargement...</p>;
+  if (state.error) return <p className='error'>Erreur: {state.error}</p>;
 
   return (
     <>
-      {data?.photographers.map((photographer, index) => (
+      {state.data?.photographers.map((photographer, index) => (
         <article className="card" key={photographer.id} tabIndex={index+1}>
           <a aria-label={`Voir le profil de ${photographer.name}`} title={`Voir le profil de ${photographer.name}`}>
             <img src={`./photographers/${photographer.portrait}`} alt={photographer.name} />
